d8b375adafcf4b6e6065442dbd6b13d0e973f4ad
Enzo Marcolino <the3brozgames@gmail.com>
2024-05-06 16:45:34 -0300

    atualização

--- fundamentos/Exercicios/simulador/logica.js
+++ fundamentos/Exercicios/simulador/logica.js
@@ -1,24 +1,68 @@
 /**
  * JS - Simulador de operadores lógicos
- * @author Enzo Marcolino
+ * @author João Victor Santos
  */
 
-// valor recebido
-// 1 acender lâmpada
-// 2 apagar lâmpada
-// 3 quebrar lâmpada
+let valor // Valor recebido
+// 1 Acender a lâmpada
+// 2 Apagar a lâmpada
+// 3 Quebrar a lâmpada
+let quebrada = false // Estado da lâmpada
 
-let valor
-let quebrada = false
+
+
+//chaves
+let sw1 = false
+let sw2 = false
+let 
 
 function simular(valor) {
+    //chaves
 
-    //quebrar a lâmpada
+
+    if (valor === 1 && sw1 === false) {
+        document.getElementById("sw1").src = "./img/swon.png"
+        sw1 = true
+    } else if (valor === 1 && sw1 === true) {
+        document.getElementById("sw1").src = "./img/swoff.png"
+        sw1 = false
+    } else if (valor === 2 && sw2 === false) {
+        document.getElementById("sw2").src = "./img/swon.png"
+        sw2 = true
+    } else if (valor === 2 && sw2 === true) {
+        document.getElementById("sw2").src = "./img/swoff.png"
+        sw2 = false
+    }
+
+
+    // Quebrar a lâmpada
     if (valor === 3) {
         let beep = new Audio()
         beep.src = "./sound/glassbreaking.wav"
         beep.play()
         document.getElementById("lamp").src = "./img/broken.jpg"
     }
-}
 
+
+    //AND
+    if (quebrada === false && path === "/exercicios/simulador/and.html"){
+        if(sw1 === true && sw2 === true) {
+            document.getElementById("lamp").src = "./img/on.jpg"
+        } else {
+            document.getElementById("lamp").src = "./img/off.jpg"
+        }
+    }
+}
+    
+     //OR
+     if (quebrada === false && path === "/exercicios/simulador/or.html"){
+        
+     }
+
+
+
+
+    //NOT
+    if (quebrada === false && path === "/exercicios/simulador/not.html"){
+
+    }
